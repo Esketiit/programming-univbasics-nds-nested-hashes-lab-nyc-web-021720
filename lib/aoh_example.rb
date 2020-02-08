c96adacdb6510cb2c5fa19a9a2891b83a7e10c92
Brian Desplantes <heftygac@gmail.com>
2020-02-08 15:13:13 +0000

    Automatically backed up by Learn

--- lib/aoh_example.rb
+++ lib/aoh_example.rb
@@ -36,7 +36,7 @@ def literal_aoh
   people.each do |person|
     temp_arr = []
     temp_arr.push(person[:name])
-    temp_arr.push(perosn[:name])
+    temp_arr.push(person[:name])
     people_arr.push(temp_arr)
   end
 
